371f4a2d2be06ac54df70de219d98dd2dae2fc9a
Jameel Alsalam <alsalam.jameel@epa.gov>
2019-09-30 13:35:36 -0400

    add 2012 <--> 2017 concordances

--- data-raw/read_concordance.R
+++ data-raw/read_concordance.R
@@ -24,3 +24,27 @@ naics_2012_2007 <- read_excel(
   select(naics_2012, naics_2007)
 
 usethis::use_data(naics_2012_2007, overwrite = TRUE)
+
+naics_2012_2017 <- read_excel(
+  "data-raw/census/2012_to_2017_NAICS.xlsx",
+  skip = 3,
+  col_names = c("naics_2012", "naics_2012_desc",
+                "naics_2017", "naics_2017_desc")
+  ,
+  col_types = c(rep("text", 4), rep("skip", 5))) %>%
+  select(naics_2012, naics_2017)
+# file has 5 extra blank columns
+
+usethis::use_data(naics_2012_2017, overwrite = TRUE)
+
+naics_2017_2012 <- read_excel(
+  "data-raw/census/2017_to_2012_NAICS.xlsx",
+  skip = 3,
+  col_names = c("naics_2017", "naics_2017_desc",
+                "naics_2012", "naics_2012_desc")
+  ,
+  col_types = c(rep("text", 4), rep("skip", 7))) %>%
+  select(naics_2017, naics_2012)
+# file has 7 extra blank columns
+
+usethis::use_data(naics_2017_2012, overwrite = TRUE)
