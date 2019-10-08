b24cc568c58695e3df09943359548c09029a60d7
Jameel Alsalam <alsalam.jameel@epa.gov>
2019-10-08 15:38:18 -0400

    initial coverage asm09

---
+++ tests/testthat/test-asm09.R
@@ -0,0 +1,13 @@
+
+library(naicsmatch)
+
+asm09 <- mutate(asm09,
+       type  = naics_code_type(naics_2007),
+       level = naics_code_level(naics_2007))
+
+test_that("ASM 09 data coverage", {
+
+  expect_false(any(is.na(asm09$level)))
+  expect_false(any(is.na(asm09$type)))
+
+})
